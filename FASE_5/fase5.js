2256040e3a62a5218820725c89538f7294e9901f
pbahadian <46781071+pbahadian@users.noreply.github.com>
2019-04-08 10:17:28 +0200

    Add files via upload

---
+++ FASE_5/fase5.js
@@ -0,0 +1,12 @@
+"use strict"
+
+var nota_examen = 10;
+
+
+if  (nota_examen >= 5) {
+	alert("Muy bien! Has aprobado el examen con un " + nota_examen);
+
+} else (nota examen < 5) {
+	alert("Ohh! Has suspendido el examen con un " + nota_examen);	
+}
+
