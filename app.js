081c62066abab4fadb8cc6ead27e6f1336cf9d42
pbahadian <46781071+pbahadian@users.noreply.github.com>
2019-04-09 12:22:49 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,115 @@
+
+/*FASE 1
+
+console.log('Hola Mundo!');
+
+*/
+
+
+/* FASE 2
+
+alert('Me llamo Paula!');
+
+*/
+
+
+/*FASE3
+
+var name;
+
+name= "Paula";
+
+var surname;
+
+surname = "Bahadian";
+
+console.log(name + " " + surname);
+
+*/
+
+/*FASE3 opción2
+
+var name;
+
+name= "Paula";
+
+var surname;
+
+surname = "Bahadian";
+
+ var concatTexto = name + " " + surname ;
+ 
+console.log( concatTexto ); 
+
+*/
+
+
+/*FASE 4
+
+"use strict"
+
+var numberA = 62;
+
+var numberB = 47;
+
+var sum = numberA + numberB;
+console.log('La suma entre '+ numberA + ' i ' + numberB + ' es ' , sum);
+
+*/
+
+
+/*FASE 5 
+
+"use strict"
+
+var nota_examen = 10;
+
+
+if  (nota_examen >= 5) {
+	alert("Muy bien! Has aprobado el examen con un " + nota_examen);
+
+} else (nota examen < 5) {
+	alert("Ohh! Has suspendido el examen con un " + nota_examen);	
+}
+*/
+
+
+/*FASE 6
+
+var str = "Tinc un cotxe de color blau.";
+
+var res = str.replace(/blau/gi, "verd"); 
+
+//var res = str.replace(/o/g,"u");
+
+console.log(res); 
+
+*/
+
+
+/*FASE 7
+
+var lista = ['taula','cadira','ordinador','libreta'];
+
+
+    for (x=0; x < lista.length; x++){
+            console.log("El objecte " + lista[x] + " esta a la posicio " + x);
+    }
+
+*/
+
+/*FASE 8
+
+var suma = function(valor1, valor2) {
+    return valor1 + valor2;
+};
+var resta = function(num1, num2) {
+    return valor1 - valor2;
+};
+var multiplica = function(num1, num2) {
+    return valor1 * valor2;
+};
+var divide = function(num1, num2) {
+    return valor1 / valor2;
+};
+ */
