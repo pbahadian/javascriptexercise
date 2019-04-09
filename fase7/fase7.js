5d0221cd75ff88c737f09523b3272b9941d46e88
pbahadian <46781071+pbahadian@users.noreply.github.com>
2019-04-09 10:15:25 +0200

    Add files via upload

---
+++ fase7/fase7.js
@@ -0,0 +1,9 @@
+
+
+var lista = ['taula','cadira','ordinador','libreta'];
+
+
+    for (x=0; x < lista.length; x++){
+            console.log("El objecte " + lista[x] + " esta a la posicio " + x);
+    }
+
