7d7971179c2ccfecaa949eefd5499aa44c56f323
pbahadian <46781071+pbahadian@users.noreply.github.com>
2019-04-09 12:09:43 +0200

    Delete fase7.js

--- fase7/fase7.js
+++
@@ -1,9 +0,0 @@
-
-
-var lista = ['taula','cadira','ordinador','libreta'];
-
-
-    for (x=0; x < lista.length; x++){
-            console.log("El objecte " + lista[x] + " esta a la posicio " + x);
-    }
-
